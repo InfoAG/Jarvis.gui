a70273b1f5f7868f55a0a1e9f111d70d1d537c67
Alexander Schlueter <arleckshunt@googlemail.com>
2012-08-25 13:17:39 +0200

    move QMLModulePackage members to private scope

--- QMLModulePackage.h
+++ QMLModulePackage.h
@@ -6,13 +6,19 @@
 #include "QMLOperatorModule.h"
 #include "QMLFunctionModule.h"
 
-struct QMLModulePackage : public QObject
+class QMLModulePackage : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QString name READ name)
     Q_PROPERTY(QList<QObject*> terminals READ terminals)
     Q_PROPERTY(QList<QObject*> operators READ operators)
     Q_PROPERTY(QList<QObject*> functions READ functions)
+
+private:
+    QString name_;
+    QList<QObject*> terminals_;
+    QList<QObject*> operators_;
+    QList<QObject*> functions_;
 
 public:
     QMLModulePackage(const ModulePackage &pkg);
@@ -21,11 +27,6 @@ public:
     QList<QObject*> terminals() const { return terminals_; }
     QList<QObject*> operators() const { return operators_; }
     QList<QObject*> functions() const { return functions_; }
-
-    QString name_;
-    QList<QObject*> terminals_;
-    QList<QObject*> operators_;
-    QList<QObject*> functions_;
 };
 
 #endif // QMLMODULEPACKAGE_H
